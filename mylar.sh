b3cadfee93d6a65de82c72590d6304102685bcc7
noinip <pinion@gmail.com>
2015-03-08 20:12:05 -0400

    Update mylar.sh

--- mylar.sh
+++ mylar.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# Copy over post processing if it isn't there
+if [ -d "/config/post-processing/" ]; then 
+	# do nothing
+  else
+	exec cp -R /opt/mylar/post-processing/ /config/
+  fi
+fi
+
 exec /sbin/setuser nobody python /opt/mylar/Mylar.py --datadir=/config
